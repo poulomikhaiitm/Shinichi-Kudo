149ba490f6b0fe741ac9d0fb6bfed1bfbe81ba98
poulomikhaiitm <50695732+poulomikhaiitm@users.noreply.github.com>
2019-05-17 13:44:39 +0530

    Add files via upload

---
+++ minutes & seconds.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 13:54:03 2019
+
+@author: shinichikudo
+"""
+
+N = int(input('Enter the value of Minutes = '))
+P = int(input('Enter the value of seconds = '))
+S = (N*60) + P
+print(S)
