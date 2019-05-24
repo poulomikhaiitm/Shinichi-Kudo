cff337942d00e1e5412550e996c371a1a5d59377
Poulomi Kha <50695732+poulomikhaiitm@users.noreply.github.com>
2019-05-24 12:57:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 11:49:59 2019
+
+@author: ShinichiKudo
+"""
+
+def add(a,b):
+        return(a+b)
+print (add(2,3))
---
+++ testmain.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 12:38:56 2019
+
+@author: ShinichiKudo
+"""
+#the result obtained here not only runs the current one but the previous script along with this
+# this can be used for printing the result up and down; combine two files
+from main import add
+print(add(2,2))
+
+ 
+
